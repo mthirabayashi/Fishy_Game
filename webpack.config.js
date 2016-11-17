4e2f646471c4fd92b5bdffa0934781172d48d946
Michael Hirabayashi <mikehirabayashi@Michaels-MacBook-Air-2.local>
2016-11-16 19:46:23 -0800

    add styling and new page layout

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,20 @@ module.exports = {
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+  loaders: [
+   {
+     test: [/\.js?$/],
+     exclude: /(node_modules)/,
+     loader: 'babel',
+     query: {
+       presets: ['es2015']
+     },
+   },
+  ]
+  },
+  resolve: {
+  extensions: ['', '.js' ]
+  },
   devtool: 'source-map',
 };
